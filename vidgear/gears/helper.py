26e9b14ad825a80be719e88d6f879701a9df8302
kimo722504 <kimo722504@gmail.com>
2020-03-24 00:38:43 +0900

    Add 'disable_existing_loggers' option in helper.py

--- vidgear/gears/helper.py
+++ vidgear/gears/helper.py
@@ -31,6 +31,7 @@ import logging.config
 #logging formatter
 logging.config.dictConfig({
 	'version': 1,
+	'disable_existing_loggers': False,
 	'formatters': {
 		'colored': {
 			'()': 'colorlog.ColoredFormatter',
